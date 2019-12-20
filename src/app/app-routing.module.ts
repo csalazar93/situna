60c3166ca9dff22712fe429757b34744af5cf4a1
csalazar93 <47618078+csalazar93@users.noreply.github.com>
2019-12-19 20:25:36 -0600

    Cambios en interfaz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,23 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { InicioComponent } from './components/inicio/inicio.component';
 import { AcercadeComponent } from './components/acercade/acercade.component';
+import { PlantaComponent } from './components/planta/planta.component';
+import { AtractivoComponent } from './components/atractivo/atractivo.component';
+import { ReportesComponent } from './components/reportes/reportes.component';
+import { AnalisisComponent } from './components/analisis/analisis.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { RolesComponent } from './components/roles/roles.component';
+
 
 //Importar los componentes para generar las rutas
 const routes: Routes = [
   { path: 'inicio', component: InicioComponent},
+  { path: 'planta', component: PlantaComponent},
+  { path: 'atractivo', component: AtractivoComponent},
+  { path: 'reportes', component: ReportesComponent},
+  { path: 'analisis', component: AnalisisComponent},
+  { path: 'usuarios', component: UsuariosComponent},
+  { path: 'roles', component: RolesComponent},
   { path: 'about', component: AcercadeComponent},
   { path: '**', pathMatch: 'full', redirectTo: 'inicio' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';//Esto se agrega para poder usar el router-outlet
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { AcercadeComponent } from './components/acercade/acercade.component';
+import { PlantaComponent } from './components/planta/planta.component';
+import { AtractivoComponent } from './components/atractivo/atractivo.component';
+import { ReportesComponent } from './components/reportes/reportes.component';
+import { AnalisisComponent } from './components/analisis/analisis.component';
+import { RolesComponent } from './components/roles/roles.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +20,17 @@ import { AcercadeComponent } from './components/acercade/acercade.component';
     NavbarComponent,
     InicioComponent,
     AcercadeComponent,
+    PlantaComponent,
+    AtractivoComponent,
+    ReportesComponent,
+    AnalisisComponent,
+    RolesComponent,
+    UsuariosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]

e5d504f32eed4177922abc15e4e1134cc8c9a755
Cristhian Salazar <christian@appy.la>
2022-04-10 20:05:42 -0600

    Cambiar version

---
+++ src/app/components/prueba/prueba.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'prueba',
+    templateUrl: 'prueba.component.html',
+    styleUrls: ['prueba.component.css']
+})
+export class PruebaComponent {
+
+}
